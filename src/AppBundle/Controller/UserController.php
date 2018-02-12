1e21b787bc2c8e6741739649ecbaf8825e2f452a
lordneru <lordneru@gmail.com>
2018-02-12 01:21:58 +0100

    añadir birra al user, eliminar y borrar

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -18,7 +18,7 @@ use AppBundle\Entity\usuario_beer_mapping;
 class UserController extends Controller
 {
     /**
-    * @Route("/")
+    * @Route("/", name="index")
     */
     public function index(Request $request){
         $usuario = new usuario; 
@@ -30,7 +30,7 @@ class UserController extends Controller
                 ->getForm();
 
         return $this->render('usuarios/index.html.twig', [
-            'loginform' => $form->createView()
+            'loginform' => $form->createView(),
             ]);
     }
     
@@ -93,12 +93,14 @@ class UserController extends Controller
             $userDao = $this->getDoctrine()->getManager();
             $userDao->persist($usuario);
             $userDao->flush();
+            
+            return $this->render('usuarios/registroCorrecto.html.twig');
         }
         
-        return $this->render('usuarios/registro.html.twig',
-                ['registerform' => $form->createView()]);
+        return $this->render('usuarios/registro.html.twig', [
+            'registerform' => $form->createView()]);
     }
-           
+             
     /**
     * @Route("/user/update")
     */
@@ -119,15 +121,10 @@ class UserController extends Controller
     public function viewCervezasPorUsuario(Request $request){
         $userName = $request->get('userName');
         $userId = $request->get('userId');
-        
-//        $response = json_decode($this->forward('AppBundle:Cerveza:getBeerByUserId', array(
-//                'userId'  => $userId
-//            )));
-//
-//        $cervezas = $response['cervezas'];
+               
         $cervezas = $this->getDoctrine()->getManager()  //getRepository('AppBundle:cerveza', 'c')
                 ->createQueryBuilder()
-                ->select('c.id', 'c.nombre', 'c.alc', 'ubm.notes')
+                ->select('c.id', 'c.nombre', 'c.alc', 'ubm.notes', 'ubm.id AS mappingId')
                 ->from('AppBundle:cerveza', 'c')
                 ->join('AppBundle:usuario_beer_mapping', 'ubm', 'WITH', 'ubm.cervezaId = c.id')
                 ->where('ubm.userId = :id')
@@ -138,7 +135,99 @@ class UserController extends Controller
         return $this->render('usuarios/verCervezas.html.twig', [
                         'userName' => $userName,
                         'userId' => $userId,
-                        'cervezas' => $cervezas
+                        'cervezas' => $cervezas,
                     ]);
+    }
+    
+    /**
+     * @Route("/user/beer/delete/{beerMappingId}"), name="user_beer_delete")
+     */
+    public function deleteBeerFromUser($beerMappingId, Request $request){
+        $dbManager = $this->getDoctrine()->getManager();
+        
+        $beerMapping = $dbManager->getRepository('AppBundle:usuario_beer_mapping')
+                 ->find($beerMappingId);
+        
+        $cerveza = $dbManager->getRepository('AppBundle:cerveza')
+                 ->find($beerMapping->getCervezaId());
+        
+        $dbManager->remove($beerMapping);
+        $dbManager->flush();
+        
+        return $this->render('cervezas/beerMappingDeleted.html.twig', [
+            'beerName' => $cerveza->getNombre(),
+            'beerNote' => $beerMapping->getNotes()
+        ]);
+    }
+    
+    /**
+    * @Route("/user/{userId}/beer/add"), name="user_beer_add_form")
+    */
+    public function generateFormToAddBeerUser($userId, Request $request){
+        $dbManager = $this->getDoctrine()->getManager();
+        
+        if(isset($_POST["beerid"])){
+            $beerId = $_POST["beerid"];
+            $notes = $_POST["notes"];
+            return $this->addBeerToUser($userId, $beerId, $notes);
+            
+        }else{
+            $cervezas = $dbManager->getRepository('AppBundle:cerveza')
+                     ->findAll();
+
+            return $this->render('cervezas/beerMappingAdd.html.twig', [
+                'userId' => $userId,
+                'cervezas' => $cervezas
+            ]);
+        }
+    }
+    
+    private function addBeerToUser($userId, $beerId, $notes){
+        $beerMapping = new usuario_beer_mapping;
+        
+        $beerMapping->setCervezaId($beerId);
+        $beerMapping->setUserId($userId);
+        $beerMapping->setNotes($notes);
+
+        $dbManager = $this->getDoctrine()->getManager();
+        $dbManager->persist($beerMapping);
+        $dbManager->flush();
+
+        return $this->redirectToRoute('user_dashboard', array(
+               'userName' => "javi",
+               'userId' => 1
+        ), 307);
+    }
+    
+    /**
+    * @Route("/user/beer/edit/{beerMappingId}"), name="user_beer_note_edit")
+    */
+    public function editBeerNote($beerMappingId, Request $request){
+        $dbManager = $this->getDoctrine()->getManager();
+        
+        $beerMapping = $dbManager->getRepository('AppBundle:usuario_beer_mapping')
+                 ->find($beerMappingId);
+                
+        $form = $this->createFormBuilder($beerMapping)
+                ->add('notes', TextType::class)
+                ->add('aceptar', SubmitType::class)
+                ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $beerMapping->setNotes($form['notes']->getData());
+            
+            //$dbManager->persist($beerMapping);
+            $dbManager->flush();
+            
+            return $this->redirectToRoute('user_dashboard', array(
+                'userName' => "javier",
+                'userId' => 1
+                ), 307);
+        }
+        
+        return $this->render('cervezas/beerMappingNoteEdit.html.twig', [
+            'editnoteform' => $form->createView()]);
     }
 }
